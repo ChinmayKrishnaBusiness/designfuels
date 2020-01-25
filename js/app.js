e43eadf9e5986f89ef4a3f4d3cd8f99bac2e8f0f
ChinmayKrishnaBusiness <chinmaykrishna.business@gmail.com>
2020-01-25 17:29:24 +0530

    phone number added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,12 @@ firebase.analytics();
 var db = firebase.firestore();
 
 var emailInput= document.querySelector("#email");
+var emailInputLabel= document.querySelector("#email-label");
+var emailSuccessMessageTop = document.querySelector("#email_success_message_top");
+var phoneInput= document.querySelector("#phone");
+var phoneInputLabel= document.querySelector("#phone-label");
 var startNowButton= document.querySelector("#start_now");
+var submitButton= document.querySelector("#submit");
 
 startNowButton.addEventListener("click", function(){
   var email= emailInput.value;
@@ -26,6 +31,38 @@ startNowButton.addEventListener("click", function(){
   })
   .then(function(docRef) {
       emailInput.value='';
+      emailInput.style.display="none";
+      emailInputLabel.style.display="none";
+      emailSuccessMessageTop.style.display="block";
+      setTimeout(function(){
+        emailSuccessMessageTop.style.display="none";
+      }, 3000);
+      phoneInputLabel.style.display="block";
+      phoneInput.style.display="block";
+      startNowButton.style.display="none";
+      submitButton.style.display="inline-block";
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+})
+
+submitButton.addEventListener("click", function(){
+  var phone= phoneInput.value;
+  db.collection("phone_numbers").add({
+      email: phone,
+
+  })
+  .then(function(docRef) {
+      phoneInput.value='';
+      phoneInput.style.display="none";
+      phoneInputLabel.style.display="none";
+      emailSuccessMessageTop.style.display="block";
+      emailSuccessMessageTop.innerHTML="Congrats ! We will contact you soon ";
+      setTimeout(function(){
+        emailSuccessMessageTop.style.display="none";
+      }, 3000);
+      submitButton.style.display="none";
   })
   .catch(function(error) {
       console.error("Error adding document: ", error);
