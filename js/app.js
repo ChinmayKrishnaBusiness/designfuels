4b7add53fe2312fc8c5976c88a51d7f44aa82580
ChinmayKrishnaBusiness <chinmaykrishna.business@gmail.com>
2020-01-25 18:59:40 +0530

    validation

--- js/app.js
+++ js/app.js
@@ -24,47 +24,57 @@ var startNowButton= document.querySelector("#start_now");
 var submitButton= document.querySelector("#submit");
 
 startNowButton.addEventListener("click", function(){
-  var email= emailInput.value;
-  db.collection("emails").add({
-      email: email,
 
-  })
-  .then(function(docRef) {
-      emailInput.value='';
-      emailInput.style.display="none";
-      emailInputLabel.style.display="none";
-      emailSuccessMessageTop.style.display="block";
-      setTimeout(function(){
-        emailSuccessMessageTop.style.display="none";
-      }, 3000);
-      phoneInputLabel.style.display="block";
-      phoneInput.style.display="block";
-      startNowButton.style.display="none";
-      submitButton.style.display="inline-block";
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });
+  var email= emailInput.value;
+  if(emailInput.checkValidity()){
+    //startNowButton.innerHTML='<img style="height:10px; width:10px;" src="svg/loading.svg">';
+    db.collection("emails").add({
+        email: email,
+
+    })
+    .then(function(docRef) {
+        emailInput.value='';
+        emailInput.style.display="none";
+        emailInputLabel.style.display="none";
+        emailSuccessMessageTop.style.display="block";
+        setTimeout(function(){
+          emailSuccessMessageTop.style.display="none";
+        }, 3000);
+        phoneInputLabel.style.display="block";
+        phoneInput.style.display="block";
+        startNowButton.style.display="none";
+        submitButton.style.display="inline-block";
+    })
+    .catch(function(error) {
+      startNowButton.innerHTML='START NOW';
+        console.error("Error adding document: ", error);
+    });
+
+  }
 })
 
 submitButton.addEventListener("click", function(){
   var phone= phoneInput.value;
-  db.collection("phone_numbers").add({
-      email: phone,
+  if(phoneInput.checkValidity()){
+    //startNowButton.innerHTML='<img style="height:10px; width:10px;" src="svg/loading.svg">';
+    db.collection("phone_numbers").add({
+        email: phone,
 
-  })
-  .then(function(docRef) {
-      phoneInput.value='';
-      phoneInput.style.display="none";
-      phoneInputLabel.style.display="none";
-      emailSuccessMessageTop.style.display="block";
-      emailSuccessMessageTop.innerHTML="Congrats ! We will contact you soon ";
-      setTimeout(function(){
-        emailSuccessMessageTop.style.display="none";
-      }, 3000);
-      submitButton.style.display="none";
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });
+    })
+    .then(function(docRef) {
+        phoneInput.value='';
+        phoneInput.style.display="none";
+        phoneInputLabel.style.display="none";
+        emailSuccessMessageTop.style.display="block";
+        emailSuccessMessageTop.innerHTML="Congrats ! We will contact you soon ";
+        setTimeout(function(){
+          emailSuccessMessageTop.style.display="none";
+        }, 3000);
+        submitButton.style.display="none";
+    })
+    .catch(function(error) {
+      submitButton.innerHTML='SUBMIT';
+        console.error("Error adding document: ", error);
+    });
+  }
 })
